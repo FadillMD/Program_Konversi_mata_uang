91f30d49580016317797c28abc90bc2f549d4d3b
FadillMD <fadillbkcc@gmail.com>
2023-03-06 20:24:43 +0700

    Test Koneksi

---
+++ src/konversi/mata/uang/KonversiMataUang.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konversi.mata.uang;
+
+public class KonversiMataUang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Konversi_mata_uang Laund = new Konversi_mata_uang();
+        Laund.setVisible(true);
+    }
+}
